b3f68d2b6e55bc03999a6d0233ac6ec086207394
Mowalky <dev@mowalky.com>
2022-08-07 21:56:36 -0500

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const myReducer = (state: {}, action: { type: string; payload: any }) => {
+  switch (action.type) {
+    case "SET_FILTER":
+      return {
+        ...state,
+        filter: action.payload,
+      };
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.payload,
+      };
+    case "SET_SELECTED_USER":
+      return {
+        ...state,
+        selectedUser: action.payload,
+      };
+    default:
+      throw new Error("no action");
+  }
+};
 
 function App() {
+  const [filter, filterSet] = React.useState("");
+  const [user, userSet] = React.useState(null);
+  const [selectedUser, selectedUserSet] = React.useState(null);
+  const [state, dispatch] = React.useReducer(myReducer, {
+    user: [],
+    filter: "",
+    selectedUser: null,
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input type="text" placeholder="Title" />
+        <input type="text" placeholder="Desc" />
+        <input type="text" placeholder="Price" />
+        <input type="text" placeholder="Category" />
+      </form>
     </div>
   );
 }
